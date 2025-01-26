4edc7ad8c144fc9ef22bf42e4924a5848d875022
Iruam Parreira <124212763+iruamfx@users.noreply.github.com>
2025-01-26 15:12:42 -0300

    Atualizar o pyrtexec.cpp 2

--- src/pyrtexec.cpp
+++ src/pyrtexec.cpp
@@ -62,42 +62,7 @@ sys.stdin = CustomInput()
         PyDict_DelItemString(builtins, "input_callback");
         Py_XDECREF(callbackFunction);
     }
-        // std::stringstream inputBuffer;
-        
-        // // Sample input to be passed to Python input() calls (you can customize this)
-        // std::string userInput = "John";  // For example, "John" as input for input()
-        
-        // // Write the user input to the stringstream (this simulates Python's input)
-        // inputBuffer << userInput << std::endl;
-
-        // // Create a custom Python file-like object for sys.stdin
-        // PyObject* ioModule = PyImport_ImportModule("io");
-        // PyObject* stringIOClass = PyObject_GetAttrString(ioModule, "StringIO");
-
-        // char* strstreamval;
-        // inputBuffer >> strstreamval;
-        // PyObject* inputBufferObject = PyObject_CallObject(stringIOClass, PyBytes_FromString(strstreamval));
-        
-        // // Set the inputBuffer as the new sys.stdin
-        // PyObject* sysModule = PyImport_ImportModule("sys");
-        // PyObject_SetAttrString(sysModule, "stdin", inputBufferObject);
-
-        // // Now execute the Python script
-        // //const char* scriptPath = scriptName.c_str();
-        // FILE* fp = fopen(scriptName, "r");
-        // if (fp) {
-        //     PyRun_SimpleFile(fp, scriptName);
-        //     fclose(fp);
-        // } else {
-        //     std::cerr << "Failed to open script: " << scriptName << std::endl;
-        // }
-
-        // // Clean up
-        // Py_XDECREF(ioModule);
-        // Py_XDECREF(stringIOClass);
-        // Py_XDECREF(inputBufferObject);
-        // Py_XDECREF(sysModule);
-//}
+}
 
 #endif
         
